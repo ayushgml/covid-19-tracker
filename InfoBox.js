474c4d74a4eccc3c10c5381bf7149bb547a6e8e0
ayushgml <72748253+ayushgml@users.noreply.github.com>
2021-07-09 21:34:24 +0530

    Add files via upload

---
+++ InfoBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './InfoBox.css';
+import {Card, CardContent, Typography} from '@material-ui/core';
+
+
+function Infobox({title, cases, total}) {
+    return (
+        <Card className="infobox">
+            <CardContent>
+                <Typography 
+                className="infobox__title" 
+                color="textSecondary">
+                    {title}
+                </Typography>
+
+                <h2 className="infobox__cases">
+                    {cases}
+                </h2>
+
+                <Typography 
+                className="infobox__total" 
+                color="textSecondary">
+                    {total} Total
+                </Typography>
+                    
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Infobox
